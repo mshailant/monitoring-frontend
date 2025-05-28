404c45c0a8b55bce77c3a83d2b2c8925a4ecb4ee
mshailant <mshailant@gmail.com>
2025-05-28 14:25:05 -0300

    added shadcd/ui

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -1,16 +1,6 @@
 import type { Metadata } from "next";
-import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
-
-const geistSans = Geist({
-  variable: "--font-geist-sans",
-  subsets: ["latin"],
-});
-
-const geistMono = Geist_Mono({
-  variable: "--font-geist-mono",
-  subsets: ["latin"],
-});
+import {ThemeProvider} from "@/components/theme-provider";
 
 export const metadata: Metadata = {
   title: "Create Next App",
@@ -24,11 +14,15 @@ export default function RootLayout({
 }>) {
   return (
     <html lang="en">
-      <body
-        suppressHydrationWarning
-        className={`${geistSans.variable} ${geistMono.variable} antialiased`}
+      <body>
+      <ThemeProvider
+          attribute="class"
+          defaultTheme="system"
+          enableSystem
+          disableTransitionOnChange
       >
-        {children}
+          {children}
+      </ThemeProvider>
       </body>
     </html>
   );
